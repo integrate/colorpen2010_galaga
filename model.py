b19e6657a0a79059d21744e281433d0db7cdec08
user <ggmodhunters@gmail.com>
2023-12-28 09:28:15 +0200

    version 1.2.8

--- model.py
+++ model.py
@@ -4,6 +4,11 @@ import playercl
 debug = False
 paint = True
 player = playercl.Playerc(530, 530)
-enemy1 = enemycl.Enemyc('original/enemy/big_green1.png', 'original/enemy/big_green2.png', 100, 100, 500, 100, 200, 15)
-enemy2 = enemycl.Enemyc('original/enemy/butterfly_red1.png', 'original/enemy/butterfly_red2.png', 200, 100, 1500, 300,
-                        400, 5)
+enemy1 = enemycl.Enemyc('original/enemy/big_green1.png', 'original/enemy/big_green2.png', 100, 100, 500, 100, 200,15)
+enemy2 = enemycl.Enemyc('original/enemy/big_green1.png', 'original/enemy/big_green2.png', 140, 100, 500, 140, 240,15)
+enemy3 = enemycl.Enemyc('original/enemy/big_green1.png', 'original/enemy/big_green2.png', 180, 100, 500, 180, 280,15)
+enemy4 = enemycl.Enemyc('original/enemy/big_green1.png', 'original/enemy/big_green2.png', 220, 100, 500, 220, 320,15)
+enemy5 = enemycl.Enemyc('original/enemy/butterfly_red1.png', 'original/enemy/butterfly_red2.png', 300, 140, 500,300, 400, 15)
+enemy6 = enemycl.Enemyc('original/enemy/butterfly_red1.png', 'original/enemy/butterfly_red2.png', 335, 140, 500,335, 435, 15)
+enemy7 = enemycl.Enemyc('original/enemy/butterfly_red1.png', 'original/enemy/butterfly_red2.png', 370, 140, 500,370, 470, 15)
+enemy8 = enemycl.Enemyc('original/enemy/butterfly_red1.png', 'original/enemy/butterfly_red2.png', 405, 140, 500,405, 505, 15)
