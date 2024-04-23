b2987cef0e4eff2898244aa37af5f71bbffb0471
user <ggmodhunters@gmail.com>
2024-04-23 10:30:29 +0300

    version 1.5.4

--- marshrutizator.py
+++ marshrutizator.py
@@ -6,6 +6,7 @@ class Marshutizator():
         self.color=color
         self.n1=nadpis.Nadpis(0,0,'build a way mode: ON',15,[255,254,7])
         self.rector=[]
+        self.points=[]
         self.numb=[]
         self.button=knopka.Knopka(10,20,'custom/Entity/кнопка.png',self.ryletka)
         self.ryletka()
@@ -28,12 +29,18 @@ class Marshutizator():
         self.button.risyem(screen)
         self.test.paint(screen)
     def ryletka(self):
-        self.rectik=pygame.rect.Rect(random.randint(10,590),random.randint(10,590),10,10)
+        self.rectik=pygame.rect.Rect(random.randint(10,580),random.randint(10,580),10,10)
         self.rector.append(self.rectik)
+        self.points.append(self.rectik.center)
         self.numb.append(nadpis.Nadpis(self.rectik.x + 4, self.rectik.y + 4, str(self.rector.index(self.rectik)), 20, [255, 0, 0]))
     def control_center(self,events):
         self.button.event(events)
+        self.test.toolgun(events)
         for o in events:
+            if o.type == pygame.KEYUP and o.key == pygame.K_s:
+                self.test.plavniy_fly_tohcy(self.rector[0].topleft, self.points)
             for j in self.rector:
+
                 if o.type == pygame.MOUSEBUTTONUP and j.collidepoint(o.pos):
                     self.vidilenie=j
+
